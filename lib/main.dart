109d426d5475ba9c47c6a48db99d7d47a05a3e16
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-06-07 23:33:57 -0400

    Configuraciones iniciales y dependencias esenciales

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:aetherium_movil/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(ProviderScope(child: const MainApp()));
@@ -10,12 +12,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        scaffoldBackgroundColor: Colors.lightGreenAccent.shade700,
+        textTheme: TextTheme(
+          displayLarge: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          titleLarge: GoogleFonts.oswald(
+            fontSize: 30,
+            fontStyle: FontStyle.italic,
+          ),
+          bodyMedium: GoogleFonts.merriweather(),
+          displaySmall: GoogleFonts.pacifico(),
         ),
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: Home(),
     );
   }
 }
